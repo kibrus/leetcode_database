debdae277814bc316204e4759d7a6cf32d933ebf
Kibru <kibrussmenore@gmail.com>
2026-02-09 16:55:27 -0600

    Time: 1251 ms (44.18%) | Memory: 0B (100.00%) - LeetSync

--- 1153-product-sales-analysis-i/product-sales-analysis-i.sql
+++ 1153-product-sales-analysis-i/product-sales-analysis-i.sql
@@ -1,5 +1,5 @@
 select
-Product.Product_name, Sales.year, Sales.price
+Product.product_name, Sales.year, Sales.price
 from Sales
-inner join Product on Sales.product_id = Product.Product_id
+left join Product on Sales.product_id = Product.Product_id
 
