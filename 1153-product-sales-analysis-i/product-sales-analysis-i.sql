fbddb408bd5fec4cde772b442303bc880c795469
Kibru <kibrussmenore@gmail.com>
2026-02-09 16:55:40 -0600

    Time: 1140 ms (79.15%) | Memory: 0B (100.00%) - LeetSync

--- 1153-product-sales-analysis-i/product-sales-analysis-i.sql
+++ 1153-product-sales-analysis-i/product-sales-analysis-i.sql
@@ -1,5 +1,5 @@
 select
 Product.product_name, Sales.year, Sales.price
 from Sales
-left join Product on Sales.product_id = Product.Product_id
+inner join Product on Sales.product_id = Product.Product_id
 
