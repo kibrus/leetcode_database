189d6b731a29f240821f384df43cefe95f3e4268
Kibru <kibrussmenore@gmail.com>
2026-02-10 12:25:19 -0600

    Time: 471 ms (78.95%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1161-project-employees-i/project-employees-i.sql
@@ -0,0 +1,7 @@
+select 
+project_id,
+round(avg(Employee.experience_years), 2) as average_years
+
+from Project
+left join Employee on Project.employee_id = Employee.employee_id
+group by project_id
